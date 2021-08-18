bea2f2658c91fe745e95550a7da3d94350cbcc8b
rgdhgdrsfdgse <89154892+rgdhgdrsfdgse@users.noreply.github.com>
2021-08-18 18:08:52 +0100

    Update IwDaTbItSrAhWsHtIgZZZLsCdWiDcMcMtIgHGUI3H44IH3IHGV348IHOI.py

--- IwDaTbItSrAhWsHtIgZZZLsCdWiDcMcMtIgHGUI3H44IH3IHGV348IHOI.py
+++ IwDaTbItSrAhWsHtIgZZZLsCdWiDcMcMtIgHGUI3H44IH3IHGV348IHOI.py
@@ -1,1 +1,151 @@
-CODE HERE
+import colorama
+from colorama import Fore, Style, Back
+import time
+import webbrowser
+import os
+import ctypes
+colorama.init()
+
+clear = lambda: os.system('cls')
+
+ver = 1.0
+
+ctypes.windll.kernel32.SetConsoleTitleW(f'Thundux v{ver} [BETA] | Loading.')
+time.sleep(0.3)
+ctypes.windll.kernel32.SetConsoleTitleW(f'Thundux v{ver} [BETA] | Loading..')
+time.sleep(0.3)
+ctypes.windll.kernel32.SetConsoleTitleW(f'Thundux v{ver} [BETA] | Loading...')
+time.sleep(0.3)
+ctypes.windll.kernel32.SetConsoleTitleW(f'Thundux v{ver} [BETA] | Loading.')
+time.sleep(0.3)
+ctypes.windll.kernel32.SetConsoleTitleW(f'Thundux v{ver} [BETA] | Launched!')
+webbrowser.open("https://discord.gg/V4kShbhMpx")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#                        .▄▄ ·  ▄▄· ▄▄▄  ▪   ▄▄▄·▄▄▄▄▄.▄▄ ·                 
+#        ▪     ▪         ▐█ ▀. ▐█ ▌▪▀▄ █·██ ▐█ ▄█•██  ▐█ ▀.     ▪     ▪     
+#         ▄█▀▄  ▄█▀▄     ▄▀▀▀█▄██ ▄▄▐▀▀▄ ▐█· ██▀· ▐█.▪▄▀▀▀█▄     ▄█▀▄  ▄█▀▄ 
+#        ▐█▌.▐▌▐█▌.▐▌    ▐█▄▪▐█▐███▌▐█•█▌▐█▌▐█▪·• ▐█▌·▐█▄▪▐█    ▐█▌.▐▌▐█▌.▐▌
+#         ▀█▄▀▪ ▀█▄▀▪     ▀▀▀▀ ·▀▀▀ .▀  ▀▀▀▀.▀    ▀▀▀  ▀▀▀▀      ▀█▄▀▪ ▀█▄▀▪
+
+
+
+
+
+
+
+
+def scriptplate():
+    script = ''
+    scriptname = ''
+    scripty = open(f"{scriptname}.txt","w")
+    scripty.write(f"{script}")
+    scripty.close()
+    print(f"                    Script Pasted!")
+    print(f"            Automatically restarting in 5s.")
+    time.sleep(5)
+    clear()
+    main()
+    optpaid()
+
+
+
+
+def s1():
+    script =  'loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Jailbreak/Jailbreak"))()'
+    scriptname = 'Jailbreak Vynixius GUI'
+    scripty = open(f"{scriptname}.txt","w")
+    scripty.write(f"{script}")
+    scripty.close()
+    print(f"                    Script Pasted!")
+    print(f"            Automatically restarting in 5s.")
+    time.sleep(5)
+    clear()
+    main()
+    optpaid()
+
+def s2():
+    script = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/racemodex/my-scripts/master/dahoodcrash", true))()'
+    scriptname = 'Da Hood Crash'
+    scripty = open(f"{scriptname}.txt","w")
+    scripty.write(f"{script}")
+    scripty.close()
+    print(f"                    Script Pasted!")
+    print(f"            Automatically restarting in 5s.")
+    time.sleep(5)
+    clear()
+    main()
+    optpaid()
+
+def s3():
+    script = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/Karz1337/ZordOverParty/main/Zord3Crack"))()'
+    scriptname = 'Da Hood Aimlock'
+    scripty = open(f"{scriptname}.txt","w")
+    scripty.write(f"{script}")
+    scripty.close()
+    print(f"                    Script Pasted!")
+    print(f"            Automatically restarting in 5s.")
+    time.sleep(5)
+    clear()
+    main()
+    optpaid()
+
+
+
+
+
+
+
+
+def main():
+    print(Fore.CYAN)
+    print(f""" {Style.BRIGHT}
+        ▄▄▄▄▄ ▄ .▄ ▄• ▄▌ ▐ ▄ ·▄▄▄▄  ▄• ▄▌▐▄• ▄ 
+        •██  ██▪▐█ █▪██▌•█▌▐███▪ ██ █▪██▌ █▌█▌▪
+         {Style.NORMAL}▐█.▪██▀▐█ █▌▐█▌▐█▐▐▌▐█· ▐█▌█▌▐█▌ ·██· 
+         ▐█▌·██▌▐▀ ▐█▄█▌██▐█▌██. ██ ▐█▄█▌▪▐█·█▌
+         {Fore.BLUE}▀▀▀ ▀▀▀  · ▀▀▀ ▀▀ █▪▀▀▀▀▀•  ▀▀▀ •▀▀ ▀▀{Fore.RESET}
+
+                 ——————————X——————————
+                     Need Support?
+                 discord.gg/V4kShbhMpx
+                 ——————————X——————————{Fore.CYAN}{Style.BRIGHT}
+               [1] Jailbreak Vynixius GUI
+                   [2] Da Hood Crash
+                  [3] Da Hood Aimlock
+                          [4]
+                          [5] 
+                          [6]
+                          [7]
+                          [8] {Fore.RESET}{Style.NORMAL}
+                 ——————————X——————————{Fore.CYAN}{Style.BRIGHT}
+            Ctrl F to search for a script!
+             """)
+
+def optpaid():
+    options = int(input(f"""                   Input script ID: """))
+
+    if options == 1:
+        s1()
+    elif options == 2:
+        s2()
+    elif options == 3:
+        s3()
+    else:
+        print("              This is not a valid option.")
+        time.sleep(2)
+main()
+optpaid()
